6cea3eef002f16918e2451b2a500eea9a671909f
J. Church <c.jacob.church@gmail.com>
2024-11-06 05:40:04 -0700

    add dfs function

---
+++ src/lib/dfs/dfs.ts
@@ -0,0 +1,23 @@
+export type Frame<T> = [T, Frame<T> | undefined];
+
+/**
+ * Given starting roots, DFS through available values
+ * by repeatedly calling `next` on a given value
+ * @param next should return an empty array if further iteration
+ *  down a potential branch is undesirable; should return `false`
+ *  if desired to short circuit the whole DFS
+ */
+export function dfs<T>(
+  roots: T[],
+  next: (node: T, frame: Frame<T>) => T[] | "stop",
+) {
+  const stack: Frame<T>[] = roots.map((r) => [r, undefined]);
+  while (stack.length) {
+    const frame = stack.pop()!;
+    const children = next(frame[0], frame);
+    if (children === "stop") {
+      break;
+    }
+    stack.push(...children.map((c) => [c, frame]) as Frame<T>[]);
+  }
+}
