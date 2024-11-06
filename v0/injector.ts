17b5ba2a668d3c0a843ac43b543df92194a2ed1a
J. Church <c.jacob.church@gmail.com>
2024-11-06 06:32:47 -0700

    setting the stage

---
+++ v0/injector.ts
@@ -0,0 +1,24 @@
+/**
+ * v0 - the interface
+ *
+ * An injector begins with its interface.
+ *
+ * `inject` <= the magic function that gives you everything you wish for
+ * `injector.get` <= `inject`s less-attractive true self
+ */
+
+let activeInjector: Injector | undefined = undefined;
+export function inject<T>(key: InjectKey<T>): T {
+  // TODO
+  return undefined as T;
+}
+
+type InjectKey<T = unknown> = Structor<T>;
+type Structor<T> = new () => T;
+
+class Injector {
+  public get<T>(key: InjectKey<T>): T {
+    // TODO
+    return undefined as T;
+  }
+}
