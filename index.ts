e33b0611d26b55bb66149049aabbb464a2f4d01f
J. Church <c.jacob.church@gmail.com>
2024-11-09 21:36:24 -0700

    export getInjectionContext

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
-export { inject, injectOptional, newInjector } from "./src/injector.ts";
+export {
+  getInjectionContext,
+  inject,
+  injectOptional,
+  newInjector,
+} from "./src/injector.ts";
 export { provide } from "./src/provider.ts";
 export { key } from "./src/providekey.ts";
